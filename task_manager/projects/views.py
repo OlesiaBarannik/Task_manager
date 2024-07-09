97e9fa14e4de4e719051964131f57e52dc1475a6
OlesiaBarannik <olesyabarannik23@gmail.com>
2024-07-09 13:11:07 +0300

    feat: Add Django views for project creation, listing, and updating

--- task_manager/projects/views.py
+++ task_manager/projects/views.py
@@ -1,7 +1,29 @@
-from django.http import HttpResponse
+from django.views import View
 from django.shortcuts import render
-# Create your views here.
+from django.http import JsonResponse, HttpResponseNotAllowed
+from .models import Project
+from urllib.parse import parse_qs
 
 
-def index(request):
-    return HttpResponse("Hello")
+class ProjectView(View):
+    def get(self, request):
+        projects = Project.objects.all()
+        return render(request, 'projects/projects.html', {'projects': projects})
+
+    def post(self, request):
+        name = request.POST.get('project')
+        new_project = Project.objects.create(name=name)
+        new_project.save()
+        return JsonResponse({'id': new_project.id, 'name': new_project.name})
+
+    def patch(self, request, project_id):
+        decoded_body = request.body.decode('utf-8')
+        parsed_data = parse_qs(decoded_body)
+        new_name = parsed_data.get('new_name', [None])[0]
+        project = Project.objects.get(id=project_id)
+        project.name = new_name
+        project.save()
+        return JsonResponse({'id': project.id, 'name': project.name})
+
+
+
