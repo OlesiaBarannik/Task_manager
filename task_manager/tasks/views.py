6a90a2898826e3ee34fd080dc0b85c4d416c9448
OlesiaBarannik <olesyabarannik23@gmail.com>
2024-07-10 01:25:31 +0300

    feat: Add CRUD operations (post, get, patch, delete) for tasks

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,7 +1,35 @@
-from django.http import HttpResponse
+import json
+from django.http import JsonResponse
+from django.views import View
+from projects.models import Project
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from tasks.models import Task
 from django.shortcuts import render
-# Create your views here.
 
+class TaskCreateView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-def indext(request):
-    return HttpResponse("Hello1")
+    def post(self, request, project_id):
+        task = Task().create_task(project_id, json.loads(request.body))
+        return JsonResponse(task.to_dict(), status=201)
+
+    def get(self, request, project_id):
+        tasks = Task.objects.filter(project_id=project_id).order_by('priority')
+        tasks_list = [task.to_dict() for task in tasks]
+        if request.headers.get('Content-Type') == 'application/json':
+            return JsonResponse(tasks_list, safe=False)
+        return render(request, 'tasks/tasks.html', {'tasks_list': tasks_list})
+
+    def patch(self, request, project_id, task_id):
+        task = Task.objects.get(id=task_id, project_id=project_id)
+        data = json.loads(request.body)
+        task.update(data)
+        return JsonResponse(task.to_dict(), status=200)
+
+    def delete(self, request, project_id, task_id):
+        task = Task.objects.get(id=task_id, project_id=project_id)
+        task.delete()
+        return JsonResponse({'status': 'success'})
