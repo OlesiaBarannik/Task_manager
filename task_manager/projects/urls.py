0c6c9b8a65638d154b34b2d3e96230c9004fa645
OlesiaBarannik <olesyabarannik23@gmail.com>
2024-07-19 00:36:32 +0300

    fix: update database schema or logic in models.py

--- task_manager/projects/urls.py
+++ task_manager/projects/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import ProjectView
+
+app_name = 'projects'  # Додайте простір імен для додатку
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='project-create'),
     path('<int:project_id>/', ProjectView.as_view(), name='project-detail'),
-
 ]
-
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import TaskCreateView
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('<int:project_id>/tasks/', TaskCreateView.as_view(), name='create_task'),
     path('<int:project_id>/tasks/<int:task_id>', TaskCreateView.as_view(), name='update_task'),
